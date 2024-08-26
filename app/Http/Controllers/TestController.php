d58d4b1726c3e3831361526b29e091a342c343aa
ShalXXXX <hibiki.a.h.0618@gmail.com>
2024-08-26 17:43:28 +0900

    DB接続

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Test;
 
 class TestController extends Controller
 {
     public function index() {
+        // Eloquent
         $values = Test::all();
+
+        $count = Test::count(); // 件数取得
+
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'bbb')->get();
+        
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'bbb')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+        
 
         // dd($values);
         return view('tests.test', compact('values'));
